eace5d134cbc082d6c8316eec3f3b3789e45baf0
Shruthi KV <Shruthi.K.V@ibm.com>
2023-01-18 21:08:14 +0530

    modal added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Meals from './components/Meals/Meals';
-
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 function App() {
   return (
     <div className="App">
+      <Cart/>
      <Header/>
      <main>
       <Meals></Meals>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import classes from './Cart.module.css';
-
+import Modal from '../UI/Modal';
 const Cart = (props) => {
     
     const cartItems = (
@@ -12,7 +12,7 @@ const Cart = (props) => {
     );
 
     return(
-        <div>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -23,7 +23,7 @@ const Cart = (props) => {
                 <button className={classes.button}>Order</button>
 
             </div>
-        </div>
+        </Modal>
     );
 }
 
