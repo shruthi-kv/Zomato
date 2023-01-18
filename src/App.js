d13c7fec1b776e6f3509e38908dc5a08f89bbbce
Shruthi KV <Shruthi.K.V@ibm.com>
2023-01-18 10:34:25 +0530

    meals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Meals from './components/Meals/Meals';
 
 import Header from './components/Layout/Header';
 function App() {
   return (
     <div className="App">
      <Header/>
+     <main>
+      <Meals></Meals>
+     </main>
     </div>
   );
 }
