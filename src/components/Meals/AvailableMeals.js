a12483dab8c17ab71fd21766e1f4c0b10ebcfbd0
Shruthi KV <Shruthi.K.V@ibm.com>
2023-01-18 12:07:42 +0530

    mealsItem

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Fragment } from "react";
 import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from './MealItem/MealItem'
 const DUMMY_MEALS = [
     {
       id: 'm1',
@@ -30,13 +31,23 @@ const DUMMY_MEALS = [
 
   const AvailableMeals =() => {
      
-    const mealList = DUMMY_MEALS.map(meals => <li>{meals.name}</li>)
+    const mealList = DUMMY_MEALS.map(meals => 
+          <MealItem  
+          key={meals.id} 
+          name = {meals.name}
+          description={meals.description}
+          price = {meals.price}
+            />
+    )
 
     return(
         <section className={classes.meals}>
-            <ul>
+          <Card>
+          <ul>
             {mealList}
             </ul>
+          </Card>
+          
         </section>
        
     );
